0e6bffab5b39038a1162639cc8c2a2f3dfc83ff7
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-10-23 15:40:46 -0500

    Add remaining button variables

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const allClearButton = document.getElementById("AC");
+const clearButton = document.getElementById("C");
+const moduloButton = document.getElementById("%");
+const divideButton = document.getElementById("/");
+const multiplyButton = document.getElementById("*");
+const subtractButton = document.getElementById("-");
+const addButton = document.getElementById("+");
+const pointButton = document.getElementById(".");
+const equalsButton = document.getElementById("=");
 const numButtons = [];
 
 for (let i = 0; i <= 9; i++) {
