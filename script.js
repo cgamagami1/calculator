8435e496b075022a92f1b215a31f9553c0518eb0
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-10-23 20:41:21 -0500

    Add inputEquation function

--- script.js
+++ script.js
@@ -8,7 +8,49 @@ const addButton = document.getElementById("+");
 const pointButton = document.getElementById(".");
 const equalsButton = document.getElementById("=");
 const numButtons = [];
+let firstOperand = "";
+let operation = "";
+let secondOperand = "";
 
 for (let i = 0; i <= 9; i++) {
     numButtons.push(document.getElementById(i));
 }
+
+function inputEquation(e) {
+    if (!isNaN(e.target.id) || e.target.id === ".") {
+        if (operation === "") {
+            if (firstOperand.length < 5) {
+                if (e.target.id !== "." || (e.target.id === "." && !firstOperand.includes("."))) {
+                    firstOperand += e.target.id;
+                }
+            }
+        }
+        else {
+            if (secondOperand.length < 5) {
+                if (e.target.id !== "." || (e.target.id === "." && !secondOperand.includes("."))) {
+                    secondOperand += e.target.id;
+                }
+            }
+        }
+    }
+    else {
+        if (firstOperand !== "" && operation === "") {
+            operation = e.target.id;
+        }
+    }
+    console.log(firstOperand + " " + operation + " " + secondOperand)
+}
+
+for (let numButton of numButtons) {
+    numButton.addEventListener("click", inputEquation);
+}
+
+//allClearButton.addEventListener("click", inputEquation);
+//clearButton.addEventListener("click", inputEquation);
+moduloButton.addEventListener("click", inputEquation);
+divideButton.addEventListener("click", inputEquation);
+multiplyButton.addEventListener("click", inputEquation);
+subtractButton.addEventListener("click", inputEquation);
+addButton.addEventListener("click", inputEquation);
+pointButton.addEventListener("click", inputEquation);
+//equalsButton.addEventListener("click", inputEquation);
